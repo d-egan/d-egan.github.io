55fd3bda9cc995ac3d5cb2e3fc1f6ae502d4faeb
d-egan <d-egan@users.noreply.github.com>
2017-04-10 19:05:10 +0100

    Added blendMode switching to test1

--- js/test1.js
+++ js/test1.js
@@ -1,10 +1,12 @@
 
 var numBoxes = 10;
 var boxes = Array(numBoxes);
+var blendModes = ['normal', 'multiply', 'screen', 'overlay', 'soft-light', 'hard- light', 'color-dodge', 'color-burn', 'darken', 'lighten', 'difference', 'exclusion', 'hue', 'saturation', 'luminosity', 'color', 'add', 'subtract', 'average', 'pin-light', 'negation', 'source- over', 'source-in', 'source-out', 'source-atop', 'destination-over', 'destination-in', 'destination-out', 'destination-atop', 'lighter', 'darker', 'copy', 'xor'];
+var blendIndex = 0;
 
-for (var i = 0; i < numBoxes; i++){
+for (var i = 0; i < numBoxes; i++) {
     var colour;
-    if (Math.floor(Math.random()*2)){
+    if (Math.floor(Math.random()*2)) {
         colour = "black";
     } else {
         colour = "white";
@@ -30,31 +32,30 @@ for (var i = 0; i < numBoxes; i++){
         this.bounds.left += this.leftInc;
         this.bounds.right -= this.leftInc;
 
-        if((this.bounds.top <= 0 && this.topInc < 0) || (this.bounds.top >= view.center.y-1 && this.topInc > 0)){
+        if((this.bounds.top <= 0 && this.topInc < 0) || (this.bounds.top >= view.center.y-1 && this.topInc > 0)) {
             this.topInc *= -1;
         }
 
-        if((this.bounds.left <= 0 && this.leftInc < 0) || (this.bounds.left >= view.center.x-1 && this.leftInc > 0)){
+        if((this.bounds.left <= 0 && this.leftInc < 0) || (this.bounds.left >= view.center.x-1 && this.leftInc > 0)) {
             this.leftInc *= -1;
         }
     }
 }
 
-function onFrame(event){
+function onFrame(event) {
      for (var i = 0; i < boxes.length; i++) {
         boxes[i].update();
     }
 }
 
-function onResize(){
+function onResize() {
     for (var i = 0; i < boxes.length; i++) {
         boxes[i].center = view.center;
     }
 }
 
-function onKeyDown(event){
-    if(event.key == 'up'){
-        
+function onKeyDown(event) {
+    if(event.key == 'up') {
         if(boxes[0].opacity < 1.0) {
             for (var i = 0; i < boxes.length; i++) {
                 boxes[i].opacity += 0.01;
@@ -62,12 +63,31 @@ function onKeyDown(event){
         }
     }
 
-    if(event.key == 'down'){
-        if(boxes[0].opacity > 0.0) {
+    if(event.key == 'down') {
+        if(boxes[0].opacity > 0.1) { // Maybe try 0.01
             for (var i = 0; i < boxes.length; i++) {
                 boxes[i].opacity -= 0.01;
             }
         }
     }
+
+   if(event.key == 'right') {
+        if(blendIndex < blendMode.length) {
+            blendIndex++;
+            for (var i = 0; i < boxes.length; i++) {
+                boxes[i].blendMode = blendMode[blendIndex];
+            }
+        }
+    }
+
+    if(event.key == 'left') {
+        if(blendIndex > 0) {
+            blendIndex--;
+            for (var i = 0; i < boxes.length; i++) {
+                boxes[i].blendMode = blendMode[blendIndex];
+            }
+        }
+    }
+ 
 
 }
